2f2ac68892c5e4386b4ca1bd83838412478ad39d
ucenik <ucenik@UCENIK_9_UCIONI>
2024-04-25 10:17:41 +0200

    Login i Register forme

--- OnlineBanking/Register.cs
+++ OnlineBanking/Register.cs
@@ -1,22 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
 using System.Data;
 using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace OnlineBanking
 {
-    public partial class Pocetna : Form
+    public partial class Register : Form
     {
-        public Pocetna()
+        public Register()
         {
             InitializeComponent();
         }
 
-        private void buttonLoginEmail_Click(object sender, EventArgs e)
+        private void buttonRegister_Click(object sender, EventArgs e)
         {
             SqlConnection conn = Konekcija.Connect();
             string query = "select count(*) from Korisnik where email = @email";
             SqlCommand cmdProvera = new SqlCommand(query, conn);
-            cmdProvera.Parameters.AddWithValue("@email", textBoxLoginEmail.Text.Trim());
-            
+            cmdProvera.Parameters.AddWithValue("@email", textBoxRegisterEmail.Text.Trim());
+
             conn.Open();
             int count = Convert.ToInt32(cmdProvera.ExecuteScalar());
             if (count > 0)
@@ -27,10 +35,10 @@ namespace OnlineBanking
             {
                 SqlCommand cmdInsert = new SqlCommand("Korisnik_Insert", conn);
                 cmdInsert.CommandType = CommandType.StoredProcedure;
-                cmdInsert.Parameters.Add("@Ime", SqlDbType.VarChar).Value = textBoxLoginIme.Text;
-                cmdInsert.Parameters.Add("@Prezime", SqlDbType.VarChar).Value = textBoxLoginPrezime.Text;
-                cmdInsert.Parameters.Add("@EMail", SqlDbType.VarChar).Value = textBoxLoginEmail.Text;
-                cmdInsert.Parameters.Add("@Sifra", SqlDbType.VarChar).Value = textBoxLoginPass.Text;
+                cmdInsert.Parameters.Add("@Ime", SqlDbType.VarChar).Value = textBoxRegisterIme.Text;
+                cmdInsert.Parameters.Add("@Prezime", SqlDbType.VarChar).Value = textBoxRegisterPrezime.Text;
+                cmdInsert.Parameters.Add("@EMail", SqlDbType.VarChar).Value = textBoxRegisterEmail.Text;
+                cmdInsert.Parameters.Add("@Sifra", SqlDbType.VarChar).Value = textBoxRegisterPass.Text;
                 cmdInsert.ExecuteNonQuery();
                 Home formahome = new Home();
                 formahome.ShowDialog();
